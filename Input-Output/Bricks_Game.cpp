0bfba18766f38692a3bf8f615825693329d01906
Aman Aswal <amanpotato11@gmail.com>
2020-06-13 16:49:30 +0530

    Create Bricks_Game.cpp

---
+++ Input-Output/Bricks_Game.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i=1;
+    cin>>n;
+    while(n!=0){
+        n=n-i;
+        if(n<1){
+            cout<<"Patlu";
+            break;
+        }
+        n=n-(i*2);
+        if(n<1){
+            cout<<"Motu";
+            break;
+        }
+        i++;
+    }
+    return 0;
+}
