2ffa34dac942a8c2af1e078a07ac7f5486b37e13
Aman Aswal <amanpotato11@gmail.com>
2020-06-14 16:37:40 +0530

    Update Ali_and_Helping_innocent_people.cpp

--- Input-Output/Ali_and_Helping_innocent_people.cpp
+++ Input-Output/Ali_and_Helping_innocent_people.cpp
@@ -9,7 +9,7 @@
      
         if(s[2]=='A' || s[2]=='E' || s[2]=='I' || s[2]=='O' || s[2]=='U' || s[2] =='Y'){
                 cout<<"invalid";
-        }Ali and Helping innocent people
+        }
         else if(((s[0]+s[1])%2==0) && ((s[3]+s[4])%2==0) && ((s[4]+s[5])%2==0) && ((s[7]+s[8])%2==0)){
                 cout<<"valid";
         }
