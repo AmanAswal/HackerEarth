28b72319e17235ac6a28f0c90b40850edb471d2f
Aman Aswal <amanpotato11@gmail.com>
2020-06-18 16:43:31 +0530

    Create Birthday_Party.cpp

---
+++ Operators/Birthday_Party.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t,n,m;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>m;
+        if(m%n==0){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            cout<<"No"<<endl;
+        }
+    }
+    return 0;
+}
+AmanAswal /
+Hacker
