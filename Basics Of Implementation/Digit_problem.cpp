07659b80c3dc6fed15776d37ef3e9e66315ed316
Aman Aswal <amanpotato11@gmail.com>
2020-06-18 10:51:37 +0530

    Create Digit_problem.cpp

---
+++ Basics Of Implementation/Digit_problem.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char x[19];
+    int k;
+    cin>>x>>k;
+    int i=0;
+    while( x[i]!='\0' && k!=0){
+        if(x[i]!='9'){
+            x[i]='9';
+            k--;
+        }
+        i++;
+    }
+    cout<<x;
+
+    return 0;
+}9999999993
