a6c3da6ea6f5078d16be5169b62fbf200ef8dce9
Aman Aswal <amanpotato11@gmail.com>
2020-06-18 17:08:28 +0530

    Create Going_to_office.cpp

---
+++ Operators/Going_to_office.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    long int d,oc,of,od,cs,cb,cm,cd;
+    long inGoing to officet onlineCost, classicCost;
+
+    cin>>d;
+    cin>>oc>>of>>od;
+    cin>>cs>>cb>>cm>>cd;
+
+    onlineCost = oc + (d-of)*od;
+    classicCost = cb + ((d/cs)*cm + d*cd);
+
+    cout<<((onlineCost<=classicCost)? "Online Taxi": "Classic Taxi");
+
+    return 0;
+}
